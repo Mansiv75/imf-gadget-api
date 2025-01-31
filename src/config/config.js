89308ce0853f2e26058b9cf44f7d19939f1e3095
Mansiv75 <mv072004@gmail.com>
2025-01-31 13:46:06 +0530

    migrations applied

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 
 module.exports = {
-    development:{
+    development: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: "postgres",
         port: process.env.DB_PORT,
-
+        // Check if the DATABASE_URL exists, else fallback to default configuration
+        url: process.env.DATABASE_URL || null,
+        dialectOptions: {
+            ssl: {
+                require: true, // Ensures SSL connection
+                rejectUnauthorized: false // Allows self-signed certs
+            }
+        }
     },
 };
